5a622b2cbb56e5e8f470385a88d94e857a9632d4
Kilian Melcher <kilian.melcher@vtex.com>
2024-06-01 12:58:41 -0300

    Run HPA controller

--- emulator.go
+++ emulator.go
@@ -1,19 +1,65 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"time"
 
-	"github.com/softchris/math"
-	podautoscaler "k8s.io/kubernetes/pkg/controller/podautoscaler"
+	"k8s.io/apimachinery/pkg/api/meta/testrestmapper"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/kubernetes/pkg/controller/podautoscaler/metrics"
+	metricsfake "k8s.io/metrics/pkg/client/clientset/versioned/fake"
+	cmfake "k8s.io/metrics/pkg/client/custom_metrics/fake"
+	emfake "k8s.io/metrics/pkg/client/external_metrics/fake"
+
+	scalefake "k8s.io/client-go/scale/fake"
+
+	hpa "k8s.io/kubernetes/pkg/controller/podautoscaler"
 )
 
+
 func main(){
-	fmt.Println("oi")
 
-	x := math.Add(1,1)
+	ctx := context.TODO()
 
-	fmt.Println(x)
+	fakeEventClient := &fake.Clientset{}
+	fakeScaleClient := &scalefake.FakeScaleClient{}
+	fakeClient := &fake.Clientset{}
+	restMapper := testrestmapper.TestOnlyStaticRESTMapper(scheme.Scheme) // legacyscheme?
 
-	podautoscaler.NewHorizontalController()
+	fakeMetricsClient := &metricsfake.Clientset{}
+	fakeCMClient := &cmfake.FakeCustomMetricsClient{}
+	fakeEMClient := &emfake.FakeExternalMetricsClient{}
+	metricsClient := metrics.NewRESTMetricsClient(
+		fakeMetricsClient.MetricsV1beta1(),
+		fakeCMClient,
+		fakeEMClient,
+	)
+	resyncPeriod := 1 * time.Second
+	informerFactory := informers.NewSharedInformerFactory(fakeClient, resyncPeriod)
+	fakeHpaInformer := informerFactory.Autoscaling().V2().HorizontalPodAutoscalers()
+	fakePodInformer := informerFactory.Core().V1().Pods()
+	downScaleStabilizationWindow := 5 * time.Minute
+	tolerance := 0.1
+	cpuInitializationPeriod := 2 * time.Minute
+	delayOfInitialReadinessStatus := 10 * time.Second
 
+	hpaController := hpa.NewHorizontalController(
+		ctx,
+		fakeEventClient.CoreV1(),
+		fakeScaleClient, 
+		fakeClient.AutoscalingV2(), 
+		restMapper, 
+		metricsClient, 
+		fakeHpaInformer, 
+		fakePodInformer, 
+		resyncPeriod, 
+		downScaleStabilizationWindow, 
+		tolerance, 
+		cpuInitializationPeriod,
+		delayOfInitialReadinessStatus,
+	)
+
+	hpaController.Run(ctx, 1)
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,12 @@ module github.com/kmmelcher/hpa-emulator
 
 go 1.22.0
 
-require github.com/softchris/math v0.2.0
+require (
+	github.com/softchris/math v0.2.0
+	github.com/stretchr/testify v1.8.4
+)
+
+require github.com/pmezard/go-difflib v1.0.0 // indirect
 
 require (
 	github.com/NYTimes/gziphandler v1.1.1 // indirect
@@ -86,11 +91,11 @@ require (
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.30.1 // indirect
+	k8s.io/api v0.30.1
 	k8s.io/apiextensions-apiserver v0.0.0 // indirect
-	k8s.io/apimachinery v0.30.1 // indirect
+	k8s.io/apimachinery v0.30.1
 	k8s.io/apiserver v0.30.1 // indirect
-	k8s.io/client-go v0.30.1 // indirect
+	k8s.io/client-go v0.30.1
 	k8s.io/cloud-provider v0.0.0 // indirect
 	k8s.io/component-base v0.30.1 // indirect
 	k8s.io/component-helpers v0.30.1 // indirect
@@ -100,7 +105,7 @@ require (
 	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
 	k8s.io/kubelet v0.0.0 // indirect
 	k8s.io/kubernetes v1.30.1
-	k8s.io/metrics v0.0.0 // indirect
+	k8s.io/metrics v0.0.0
 	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
 	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.29.0 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
